e6031fc05c1f296e2b15ba0c68a8534eb0a84855
cliffgor <clierozigy@gmail.com>
2021-01-21 20:39:02 +0300

    Refactored the code

--- src/searchForm.js
+++ src/searchForm.js
@@ -7,16 +7,14 @@ export default function SearchForm({ params, onParamChange  }) {
        <Form className="mb-4">
            <Form.Row className="align-items-end">
            <Form.Group as={Col}>
-          <Form.Label>Description</Form.Label>
+          <Form.Label>Repositorie Name</Form.Label>
           <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
         </Form.Group>
         <Form.Group as={Col}>
-          <Form.Label>Location</Form.Label>
+          <Form.Label>Repositorie Language</Form.Label>
           <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
         </Form.Group>
-        <Form.Group as={Col} xs="auto" className="ml-2">
-          <Form.Check onChange={onParamChange} value={params.remote} name="remote" id="remote" label="Only Remote" type="checkbox" className="mb-2" />
-        </Form.Group>
+       
            </Form.Row>
        </Form>
     )
